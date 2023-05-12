d5b98a51465e97c0dd724d857ea982b8082dc532
Pulkit Jalani <webtecso.team@gmail.com>
2023-05-12 22:40:03 +0530

    Cancel Report Revert

--- application/modules/admin/views/report/view_cancel_report.php
+++ application/modules/admin/views/report/view_cancel_report.php
@@ -145,7 +145,7 @@
             </div>
         </div>
     </div>
-    <!--<div class="panel panel-flat">
+    <div class="panel panel-flat">
         <div class="panel-body">
             <div class="post-list" <?php if(empty($report_details)){ ?> style="padding-top:20px" <?php } ?> >
                 <div class="table-responsive table-spraye" style="min-height: 0px">
@@ -213,7 +213,7 @@
                 </div>
             </div>
         </div>
-    </div>-->
+    </div>
 </div>
 </div>
 <div class="loading" style="display:none;">
