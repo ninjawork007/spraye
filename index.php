c2ecc166ae6f13bab5160d861e1b41fa9132204c
Pulkit Jalani <webtecso.team@gmail.com>
2023-05-24 15:35:50 +0530

    Memory Increase

--- index.php
+++ index.php
@@ -73,16 +73,19 @@ require_once 'vendor/autoload.php';
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
+
+ini_set('memory_limit', '-1');
+
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
+		error_reporting(0);
 		ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
 	case 'production':
-		ini_set('display_errors', 1);
+		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
 			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
