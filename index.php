f0476af9d2ad28c80b6b9e4e5df51d8237c7f71a
Pulkit Jalani <webtecso.team@gmail.com>
2023-05-24 16:17:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -79,13 +79,13 @@ ini_set('memory_limit', '-1');
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
+		error_reporting(0);
 		ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
 	case 'production':
-		ini_set('display_errors', 1);
+		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
 			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
